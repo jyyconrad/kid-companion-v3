85d6efb502d6e67eeba840ab541dc4de9c9267f8
DevBot <dev@kidcompanion.com>
2026-02-22 19:25:34 +0800

    fix: 移除 OTAUpdate 组件（依赖已删除的 expo-updates）

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { AppNavigator } from './src/navigation/AppNavigator';
-import { OTAUpdate } from './src/components/OTAUpdate';
 
 export default function App() {
   return (
     <>
-      <OTAUpdate autoUpdate={true} checkInterval={5 * 60 * 1000} />
       <AppNavigator />
       <StatusBar style="auto" />
     </>
