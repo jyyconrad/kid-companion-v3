7c4af5699dae1201c02bf661144bb00c29ff9a85
DevBot <dev@kidcompanion.com>
2026-02-22 18:43:58 +0800

    feat: 添加 app.config.js 配置 minSdkVersion 24

---
+++ app.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+
+/**
+ * @type {import('expo').ConfigFunction}
+ */
+export default () => ({
+  name: "kid-companion-v3",
+  slug: "kid-companion-v3",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.anonymous.kidcompanionv3"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+    package: "com.anonymous.kidcompanionv3",
+    minSdkVersion: 24
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+    bundler: "metro"
+  },
+  plugins: [
+    "expo-router",
+    "expo-font"
+  ],
+  updates: {
+    url: "https://u.expo.dev/6a820012-a4e6-46de-b580-fd84d252f379"
+  },
+  extra: {
+    router: {},
+    eas: {
+      projectId: "6a820012-a4e6-46de-b580-fd84d252f379"
+    }
+  }
+});
